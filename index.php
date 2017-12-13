703261198488008fd68d8f16f052e2ccff03e3cf
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-13 10:16:05 +0100

    Création de l'exercice 7

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 7</title>
+  </head>
+  <body>
+    <?php
+    $line_number = 1;
+    while($line_number <= 100) {
+      $line_number = $line_number + 15;
+      echo 'On tient le bon bout ! ';
+    }
+    ?>
+  </body>
+</html>
